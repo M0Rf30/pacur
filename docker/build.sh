9e9dcf269486426eba84fb7bc6587c2bc7fbba7f
Gianluca Boiano <gianluca.boiano@zextras.com>
2020-12-09 17:58:01 +0100

    Change travis pipeline to my needs

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,11 @@
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+if [ -z "${TRAVIS_TAG}"  ]; then
+    TRAVIS_TAG="latest"
+fi
+
+
 for dir in */ ; do
     cd $dir
     sudo docker build --rm -t m0rf30/pacur-${dir::-1}:${TRAVIS_TAG} .
--- docker/push.sh
+++ docker/push.sh
@@ -4,6 +4,10 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+if [ -z "${TRAVIS_TAG}"  ]; then
+    TRAVIS_TAG="latest"
+fi
+
 for dir in */ ; do
     docker push m0rf30/pacur-${dir::-1}:$TRAVIS_TAG
 done
